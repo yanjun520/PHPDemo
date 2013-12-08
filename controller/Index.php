58816f436916a63f50b1845a1becba08b1c17a80
yanjun520 <bingxingxp3@gmail.com>
2013-12-08 14:20:49 +0800

    1

--- controller/Index.php
+++ controller/Index.php
@@ -4,7 +4,7 @@ class Controller_Index extends Lib_Base_Controller {
 	
 	public function index() {
 		$this->setTpl('index.tpl');
-		$msg = isset($this->get['info']) ? '' : $this->get['info'];
+		$info = isset($this->get['info']) ? $this->get['info'] : '';
 		$this->assignPage(array('info' => $info));
 	}
 	
--- template/index.tpl
+++ template/index.tpl
@@ -4,6 +4,6 @@
 <title>Net World</title>
 </head>
 <body>
-<h1>从GET参数中取值：msg={%$info%}</h1>
+<h1>从GET参数中取值：info={%$info%}</h1>
 </body>
 </html>
